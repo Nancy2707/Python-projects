7bbb5479cd1ebbc78960086f7642376ad3289f61
Nancy Sharma <nancysharma2000a@gmail.com>
2024-04-02 17:39:08 +0530

    Update QRCode.py

--- QRCode.py
+++ QRCode.py
@@ -1,17 +1,17 @@
-//Installing package qrcode
+#Installing package qrcode
 pip install qrcode
-//importing qrcode into our code
+#importing qrcode into our code
 import qrcode as qr 
-//qrcode package is aliased as qr
-//importing required packages
+#qrcode package is aliased as qr
+#importing required packages
 from PIL import image
 import qrcode as qr
 from PIL import Image
-//using QRCode feature of qrcode package to give specifications to our QRcode
+#using QRCode feature of qrcode package to give specifications to our QRcode
 QR=qr.QRCode(version=1,error_correction=qr.constants.ERROR_CORRECT_H,box_size=15,border=4)
-//adding the data or the link which will be generated in the form of QRCode
+#adding the data or the link which will be generated in the form of QRCode
 QR.add_data("www.linkedin.com/in/nancy-sharma2707")
 QR.make(fit=True)
 img=QR.make_image(fill_color="green")
-//giving the name of the image with extension which we want our qrcode to store as
+#giving the name of the image with extension which we want our qrcode to store as
 img.save("linkedin.jpg")
